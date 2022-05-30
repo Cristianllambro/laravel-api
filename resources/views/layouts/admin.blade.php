214e3739c10135de6106e812c7e6f5d810d7acc7
cristianllambro <llambro.cristian1996@gmail.comm>
2022-05-30 17:01:37 +0200

    asdsa

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name', 'BooleanPost')}} | @yield('pageTitle')</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/frontOffice.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
